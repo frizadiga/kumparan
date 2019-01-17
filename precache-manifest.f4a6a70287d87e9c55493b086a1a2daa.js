be6e5030e384acad379365eed0641fd20218bc38
frizadiga <frizadiga@gmail.com>
2019-01-17 17:17:00 +0700

    Updates

---
+++ precache-manifest.f4a6a70287d87e9c55493b086a1a2daa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bdec7634a3f545a02293",
+    "url": "/kumparan/static/js/runtime~main.bdec7634.js"
+  },
+  {
+    "revision": "6c804a99d1c1dc62f199",
+    "url": "/kumparan/static/js/main.6c804a99.chunk.js"
+  },
+  {
+    "revision": "c5b414fdfa7fee3913f1",
+    "url": "/kumparan/static/js/1.c5b414fd.chunk.js"
+  },
+  {
+    "revision": "6c804a99d1c1dc62f199",
+    "url": "/kumparan/static/css/main.956fc907.chunk.css"
+  },
+  {
+    "revision": "c08b2dd75e3904added94862d6689e70",
+    "url": "/kumparan/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.83a565d83bf5cf97ad62cd9dffa985ec.js"
+  "/kumparan/precache-manifest.f4a6a70287d87e9c55493b086a1a2daa.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/kumparan/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
