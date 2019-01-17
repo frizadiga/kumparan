e2c0c1dd25fa8711f4a98230b430a9727e7c2dd9
frizadiga <frizadiga@gmail.com>
2019-01-17 17:11:28 +0700

    Updates

---
+++ precache-manifest.83a565d83bf5cf97ad62cd9dffa985ec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a55625a56d47ad627529",
+    "url": "/static/js/main.a55625a5.chunk.js"
+  },
+  {
+    "revision": "c5b414fdfa7fee3913f1",
+    "url": "/static/js/1.c5b414fd.chunk.js"
+  },
+  {
+    "revision": "a55625a56d47ad627529",
+    "url": "/static/css/main.956fc907.chunk.css"
+  },
+  {
+    "revision": "4e63f131cc200a1fe2c5745636c15dd8",
+    "url": "/index.html"
+  }
+];
